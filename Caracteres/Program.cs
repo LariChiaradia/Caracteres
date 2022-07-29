13945e1faadb8f812937555efd7f2274ad413e78
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2022-07-29 00:43:22 -0300

    feat: add inversão de string

--- Caracteres/Program.cs
+++ Caracteres/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Escreva Algo");
+var text = Console.ReadLine();
+
+if (string.IsNullOrEmpty(text))
+    throw new Exception("Valor escrito é nulo");
+
+var tamanho = text.Length;
+var charInverter = new char[tamanho];
+
+tamanho -= 1;
+foreach (var caracter in text)
+{
+    charInverter[tamanho] = caracter;
+    tamanho--;
+}
+
+var result = new string(charInverter);
+
+Console.WriteLine("\nInverso:");
+Console.WriteLine(result);
